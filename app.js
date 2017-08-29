ca39ee1306c029c27e6a96c073bcd8b774634451
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-29 12:44:03 -0400

    added the mongoose dependency; started config for mongo in the docker-compose.yml file (WIP)

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 
 var express = require('express'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
+
+// Database setup
+mongoose.connect('mongodb://mongo:27017');
 
 app.get('/', function(req, res) {
     res.send('Hello, World!');
