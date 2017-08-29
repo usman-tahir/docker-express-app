4ffe9b01bd7979107f6afe81dcfc395b00ff1259
usman-tahir <tahir.usman.ali94@outlook.com>
2017-08-29 12:21:03 -0400

    added a Dockerfile for managing the app in a Docker container

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000, function() {
-    console.log('Listening on http://localhost:3000')
+    console.log('Listening on http://localhost:3000');
 });
